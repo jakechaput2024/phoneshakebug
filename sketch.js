885f7d0a7056746224fb6f412079993a59995d5c
JAKE CHAPUT <JWC21@FSU.EDU>
2024-09-30 23:48:48 -0400

    added p5.sound.min.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,4 @@
-// An example showing how to use a phone's accelerometer with p5.js
-
-// The app has two modes, which are toggled by shaking the phone:
-// 1. Rotation mode: the box rotates around the x-axis according to the phone's rotation
-// 2. Translation mode: the box moves along the x-axis according to the phone's acceleration
-
-let rotationMode = true; // keep track of the mode
-
-let img;
+let recentlyShaken = false;
 let baby;
 let monster;
 
@@ -20,31 +12,25 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(1000, 1000, WEBGL); // full screen canvas, 3D mode
+    createCanvas(1000, 1000);
     setShakeThreshold(50); // set the shake-strength threshold to 50, the default is 30
 }
 
 function draw() {
     background(250);
 
-    // rotate or move the box, depending on the mode
-    if (rotationMode) rotateX(radians(rotationX));
-    else translate(-accelerationX*4, 0);
-    
-    image(baby, -500, -500)
+    if(recentlyShaken) {
+        image(monster, 0, 0)
+    } else {
+        image(baby, 0, 0)
+    }
 }
 
 // this function is called when the phone is shaken beyond the setShakeThreshold value
-function deviceShaken() {
+function mouseClicked() {
     // don't continue if the phone hasn't been shaken long enough
-    if (shakes++ < mininumShakes) return;
+    //if (shakes++ < mininumShakes) return;
 
-    if (rotationMode) {
-        rotationMode = false;
-        img = "baby";
-    } else {
-        rotationMode = true;
-        img = "monster";
-    }
-    shakes = 0; // reset the number of shakes for the next time shakes are detected
+    recentlyShaken = ! recentlyShaken;
+    shakes = 0;
 }
