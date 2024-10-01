bcac40db500bc204eea3371db87503a46e6eb0a1
JAKE CHAPUT <JWC21@FSU.EDU>
2024-10-01 13:19:36 -0400

    soundeffectsadded

--- sketch.js
+++ sketch.js
@@ -1,19 +1,25 @@
 let recentlyShaken = false;
 let baby;
 let monster;
+let soundBabyCrying;
+let soundMonsterCrying;
 
 // while there is a setShakeThreshold() function, it detects the *force* of the shake, not the duration
 let shakes = 0; // keep track of the number of shakes so we can switch modes only after a certain *duration*
 let mininumShakes = 5; // the minimum number of detected shakes before the mode switches (the duration of shaking)
 
+
 function preload() {
     baby = loadImage('assets/ManeaterBaby.png');
     monster = loadImage('assets/ManeaterCel01 copy.png');
+    soundBabyCrying = loadSound('assets/Babycry2.mp3')
+    soundMonsterCrying = loadSound('assets/Fakebabycry.mp3')
 }
 
 function setup() {
     createCanvas(1000, 1000);
     setShakeThreshold(50); // set the shake-strength threshold to 50, the default is 30
+
 }
 
 function draw() {
@@ -28,9 +34,7 @@ function draw() {
 
 // this function is called when the phone is shaken beyond the setShakeThreshold value
 function mouseClicked() {
-    // don't continue if the phone hasn't been shaken long enough
-    //if (shakes++ < mininumShakes) return;
-
+   
     recentlyShaken = ! recentlyShaken;
     shakes = 0;
 }
