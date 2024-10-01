6aa274f42978e13f6310603eeb3d76cbb2a90919
JAKE CHAPUT <JWC21@FSU.EDU>
2024-10-01 19:47:32 -0400

    canvaspressedtest

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let recentlyShaken = false;
 let baby;
 let monster;
-let soundBabyCrying;
-let soundMonsterCrying;
+let sound1;
+let sound2;
 
 // while there is a setShakeThreshold() function, it detects the *force* of the shake, not the duration
 let shakes = 0; // keep track of the number of shakes so we can switch modes only after a certain *duration*
@@ -12,29 +12,34 @@ let mininumShakes = 5; // the minimum number of detected shakes before the mode 
 function preload() {
     baby = loadImage('assets/ManeaterBaby.png');
     monster = loadImage('assets/ManeaterCel01 copy.png');
-    soundBabyCrying = loadSound('assets/Babycry2.mp3');
-    soundMonsterCrying = loadSound('assets/Fakebabycry.mp3');
+    sound1 = loadSound('assets/Babycry2.mp3');
+    sound2 = loadSound('assets/Fakebabycry.mp3');
 }
 
 function setup() {
     createCanvas(1000, 1000);
     setShakeThreshold(50); // set the shake-strength threshold to 50, the default is 30
+    let cnv = createCanvas(1000, 1000);
+
+    cnv.mousePressed(canvasPressed);
 
 }
 
 function draw() {
     background(250);
 
-    if(recentlyShaken) {
+    if(canvasPressed) { //deviceShaken
         image(monster, 0, 0)
     } else {
         image(baby, 0, 0)
+        sound1.play();
     }
 }
 
 // this function is called when the phone is shaken beyond the setShakeThreshold value
-function deviceShaken() { //deviceShaken
-   
-    recentlyShaken = ! recentlyShaken;
+function canvasPressed() { //deviceShaken
+    canvasPressed = ! canvasPressed; //deviceShaken = ! deviceShaken
     shakes = 0;
+
+
 }
