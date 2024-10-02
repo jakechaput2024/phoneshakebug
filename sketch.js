78d0fafdc38fc944502f228256cc4694e9232cb7
JAKE CHAPUT <JWC21@FSU.EDU>
2024-10-02 11:36:30 -0400

    corrected a mistake, function is now "deviceshaken"

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function draw() {
 }
 
 // this function is called when the phone is shaken beyond the setShakeThreshold value
-function recentlyShaken() {
+function deviceshaken() {
    
     recentlyShaken = ! recentlyShaken;
     shakes = 0;
