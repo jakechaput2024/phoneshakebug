440823ff253816540235c5b8a2d8255e928314a1
JAKE CHAPUT <JWC21@FSU.EDU>
2024-10-02 10:38:35 -0400

    IT WORKS

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let baby;
 let monster;
 let sounds = []; //empty array
 let i = 0; //index created
-let sound1
+//let sound2
 
 // while there is a setShakeThreshold() function, it detects the *force* of the shake, not the duration
 let shakes = 0; // keep track of the number of shakes so we can switch modes only after a certain *duration*
@@ -13,8 +13,8 @@ let mininumShakes = 5; // the minimum number of detected shakes before the mode 
 function preload() {
     baby = loadImage('assets/ManeaterBaby.png');
     monster = loadImage('assets/ManeaterCel01 copy.png');
-    sound1 = loadSound('assets/sound1.mp3');
-    sounds[1] = loadSound('assets/sound2.mp3')
+    sounds[0] = loadSound('assets/sound1.mp3');
+    sounds[1] = loadSound('assets/sound2.mp3');
 }
 
 
@@ -39,7 +39,8 @@ function mouseClicked() {
    
     recentlyShaken = ! recentlyShaken;
     shakes = 0;
-    sound1.play();
+    sounds[i = (i + 1) % 2].play();
+    //sound2.play();
 
     //if ( key != 0 && key != 1 && key != 2  && key !=3) return; //if the key isn't any one of these variables, jump out of function
     //index = key;
